908c3568abfbb24f874bfa6a42388e7a635b4ac1
Jamie Gaskins <jgaskins@hey.com>
2025-04-23 12:41:54 -0500

    Show where spec failures actually are

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -277,16 +277,16 @@ module ClientSpec
   end
 end
 
-private def test_with_index(name, *, primary_key : String? = nil, **options, &block : Meilisearch::Index, Meilisearch::Client ->)
-  test name, **options do |index_uid, client|
+private def test_with_index(name, *, primary_key : String? = nil, file = __FILE__, line = __LINE__, **options, &block : Meilisearch::Index, Meilisearch::Client ->)
+  test name, **options, file: file, line: line do |index_uid, client|
     index = client.indexes.create!(index_uid, primary_key: primary_key)
 
     block.call index, client
   end
 end
 
-private def test(name, **options, &block : String, Meilisearch::Client ->)
-  it name, **options do
+private def test(name, file = __FILE__, line = __LINE__, **options, &block : String, Meilisearch::Client ->)
+  it name, **options, file: file, line: line do
     client = Meilisearch::Client.new
 
     uid = UUID.v4.to_s
